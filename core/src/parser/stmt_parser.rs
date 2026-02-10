efa9b076df685a5300e1c0cbe167e1ffd1d0b4e6
1jmdev <me@1jm.dev>
2026-02-10 09:56:36 +0100

    refactor: split statement terminator parsing helper

--- core/src/parser/stmt_parser.rs
+++ core/src/parser/stmt_parser.rs
@@ -225,20 +225,4 @@ impl Parser {
         self.consume_stmt_terminator()?;
         Ok(Stmt::Break)
     }
-
-    fn consume_stmt_terminator(&mut self) -> Result<(), SyntaxError> {
-        if self.check(&TokenKind::Semicolon) {
-            self.advance();
-            return Ok(());
-        }
-        if self.check(&TokenKind::RightBrace) || self.check(&TokenKind::Eof) {
-            return Ok(());
-        }
-        let token = self.tokens[self.pos].clone();
-        Err(SyntaxError::new(
-            format!("expected Semicolon, found {:?}", token.kind),
-            token.span.start,
-            token.span.len().max(1),
-        ))
-    }
 }
---
+++ core/src/parser/stmt_terminator.rs
@@ -0,0 +1,21 @@
+use super::Parser;
+use crate::errors::SyntaxError;
+use crate::lexer::token::TokenKind;
+
+impl Parser {
+    pub(crate) fn consume_stmt_terminator(&mut self) -> Result<(), SyntaxError> {
+        if self.check(&TokenKind::Semicolon) {
+            self.advance();
+            return Ok(());
+        }
+        if self.check(&TokenKind::RightBrace) || self.check(&TokenKind::Eof) {
+            return Ok(());
+        }
+        let token = self.tokens[self.pos].clone();
+        Err(SyntaxError::new(
+            format!("expected Semicolon, found {:?}", token.kind),
+            token.span.start,
+            token.span.len().max(1),
+        ))
+    }
+}
